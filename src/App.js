b19012e8946d28bc6bea44d99f2e2a39a2b13c13
hlasensky <tomas.hlasensky@seznam.cz>
2021-04-28 00:12:58 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -8,78 +8,10 @@ import Projects from "./components/projects/projects";
 import ErrorBoundry from "./components/ErrorBoundry"
 import Footer from "./components/footer/footer";
 
-
-import Particles from 'react-particles-js';
-
-
 import React, { Component } from 'react';
 
 
-const particlesOptions1 = {
-        "fps_limit": 29,
-        "particles": {
-            "collisions": {
-                "enable": false
-            },
-            "number": {
-                "value": 320,
-                "density": {
-                    "enable": false
-                }
-            },
-            "line_linked": {
-                "enable": true,
-                "distance": 33,
-                "opacity": 0.9
-            },
-            "move": {
-                "speed": 0.3
-            },
-            "opacity": {
-                "anim": {
-                    "enable": true,
-                    "opacity_min": 0.05,
-                    "speed": 1,
-                    "sync": false
-                },
-                "value": 0.4
-            }
-        },
-        "polygon": {
-            "enable": true,
-            "scale": 1.8,
-            "type": "inline",
-            "move": {
-                "radius": 5
-            },
-            "url": "/tree3.svg",
-            "inline": {
-                "arrangement": "equidistant"
-            },
-            "draw": {
-                "enable": true,
-                "stroke": {
-                    "color": "rgba(255, 255, 255, .2)"
-                }
-            }
-        },
-        "retina_detect": false,
-        "interactivity": {
-            detectsOn: "window",
-                "events": {
-                    "onhover": {
-                        "enable": true,
-                        "mode": "bubble"
-                    }
-            },
-            "modes": {
-                "bubble": {
-                    "size": 6,
-                    "distance": 50
-                }
-            }
-        }
-}
+
 
 
 
