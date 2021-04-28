fd21285ddff97da6b0131185910200e53fa1fb36
hlasensky <tomas.hlasensky@seznam.cz>
2021-04-28 20:17:29 +0200

    fixing globe bug

--- src/components/globe/globe.js
+++ src/components/globe/globe.js
@@ -4,20 +4,34 @@ import React from 'react'
 
 
 const Globe = () => {
-    const width = React.useState(window.innerWidth);
+    const [width, setWidth] = React.useState(window.innerWidth);
     const breakpoint = 900;
+    React.useEffect(() => {
+        const handleResizeWindow = () => setWidth(window.innerWidth);
+         // subscribe to window resize event "onComponentDidMount"
+         window.addEventListener("resize", handleResizeWindow);
+         return () => {
+             <div>
+                 
+             </div>
+           // unsubscribe "onComponentDestroy"
+           window.removeEventListener("resize", handleResizeWindow);
+         };
+       }, []);
     if (width > breakpoint) {
         return (
             <div id="globe">
                 <iframe width="100%" height="100%" src="https://cybermap.kaspersky.com/en/widget/dynamic/dark" frameBorder="0" title="background"></iframe>
             </div>
         );
-    } 
-    return (
+    } else {
+        return (
         <div>
 
         </div>
       );
+    }
+
 };
 
 export default Globe;
