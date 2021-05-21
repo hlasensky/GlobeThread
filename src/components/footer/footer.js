f4f1115e21aba236b4c280187d4af33ecc412227
hlasensky <tomas.hlasensky@seznam.cz>
2021-05-21 23:08:11 +0200

    making form working

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
 import './footer.css'
 
-const footer = () => {
+const Footer = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            e.target.reset()
+    }
+
     return (
         <section id="Contacts" className="div-contacts">
             <h2 className="contacts-h2">Contacts</h2>
@@ -14,7 +27,7 @@ const footer = () => {
                         <div className="wraper"><img className="contact-icon" src="./instagram.png" alt="error"/><h2 className="inl">@_hlasensky_</h2> </div>
                         <div className="wraper"><img className="contact-icon" src="./email.svg" alt="error"/><h2 className="inl">tomas.hlasensky@seznam.cz</h2> </div>
                     </div>
-                    <form className="contact-form" name="contact">
+                    <form className="contact-form" name="contact" onSubmit={sendEmail}>
                         <div className="contact-inputs">
                             <input required className="contact-name" name="name" placeholder="Your Name" type="text"/>
                             <input required className="contact-email" name="email" placeholder="Your Email" type="email"/>
@@ -34,4 +47,4 @@ const footer = () => {
     );
 }
 
-export default footer;
+export default Footer;
