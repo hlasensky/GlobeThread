f57eaaa719202c3de8ca190cf193e6080afce927
hlasensky <tomas.hlasensky@seznam.cz>
2021-05-22 14:23:07 +0200

    fixing form

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     function sendEmail(e) {
         e.preventDefault();
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+        emailjs.sendForm(process.env.REACT_APP_YOUR_SERVICE_ID, process.env.REACT_APP_YOUR_TEMPLATE_ID, e.target, process.env.REACT_APP_YOUR_USER_ID)
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
