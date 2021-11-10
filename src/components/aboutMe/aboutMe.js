849f63f5843232eddee68d26335d4d38ebdfb461
hlasensky <tomas.hlasensky@seznam.cz>
2021-11-10 20:07:43 +0100

    updating text

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -1,38 +1,70 @@
-import React from 'react';
+import React from "react";
 
-import './aboutMe.css'
+import "./aboutMe.css";
 
 const aboutMe = () => {
-    return (
-        <div className="about-div">
-            <section id="AboutMe">
-                <div className="clp-div">
-                    <div className="img-div">
-                        <img className="me" src="./me.jpg" alt="Me" />
-                    </div>
-                </div>
-                <div className="container">
-                    <h2 className="about-h2">About me</h2>
-                    <p className="about-p">I'm person that love doing interesting things and learning something new, also I'm self-taught programmer and web developer seeking experience and fun.</p>
-                    <div>
-                        <hr></hr>
-                        <h3 className="about-h3">Skills</h3>
-                        <div className="skill-container">
-                            <img title="Python" className="pr-lg" src="./python.png" alt="error"/>
-                            <img title="JavaScript" className="pr-lg" src="./javascript.png" alt="error"/>
-                            <img title="NodeJs" className="pr-lg" src="./node_js.svg" alt="error"/>
-                            <img title="React" className="pr-lg " src="./atom.svg" alt="error"/>   
-                            <img title="HTML" className="pr-lg" src="./html.png" alt="error"/>
-                            <img title="CSS" className="pr-lg" src="./css.png" alt="error"/>
-                            
-                        </div>
-                    </div>
-                    <img className="me2" src="./me.jpg" alt="Me" /> 
-                </div>
-            </section>    
-        </div>
-        
-    );
-}
+	return (
+		<div className="about-div">
+			<section id="AboutMe">
+				<div className="clp-div">
+					<div className="img-div">
+						<img className="me" src="./me.jpg" alt="Me" />
+					</div>
+				</div>
+				<div className="container">
+					<h2 className="about-h2">About me</h2>
+					<p className="about-p">
+						I'm a person who likes to do interesting things and
+						learn something new, I'm also a self-taught programmer
+						and web developer looking for experience and fun.
+					</p>
+					<div>
+						<hr></hr>
+						<h3 className="about-h3">Skills</h3>
+						<div className="skill-container">
+							<img
+								title="Python"
+								className="pr-lg"
+								src="./python.png"
+								alt="error"
+							/>
+							<img
+								title="JavaScript"
+								className="pr-lg"
+								src="./javascript.png"
+								alt="error"
+							/>
+							<img
+								title="NodeJs"
+								className="pr-lg"
+								src="./node_js.svg"
+								alt="error"
+							/>
+							<img
+								title="React"
+								className="pr-lg "
+								src="./atom.svg"
+								alt="error"
+							/>
+							<img
+								title="HTML"
+								className="pr-lg"
+								src="./html.png"
+								alt="error"
+							/>
+							<img
+								title="CSS"
+								className="pr-lg"
+								src="./css.png"
+								alt="error"
+							/>
+						</div>
+					</div>
+					<img className="me2" src="./me.jpg" alt="Me" />
+				</div>
+			</section>
+		</div>
+	);
+};
 
 export default aboutMe;
